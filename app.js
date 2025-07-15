deafc1f25dc514e04f34bae4d5aa466c1c159dde
rayssa-carasilo <rayssa.carasilo@mindsdigital.net>
2025-07-14 21:50:46 -0300

    [trabalhoII]: importando node,mongoose,dotenv etc

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+require("./db");
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Servidor está rodando na porta: ${port}`);
+    });
